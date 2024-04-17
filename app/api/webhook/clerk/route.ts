a75dd85e9ff0d6d0753554a50fc20633a10f9c22
nthapa000 <nishantthapa0000@gmail.com>
2024-04-17 16:53:38 +0530

    Changes

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -55,7 +55,7 @@ export async function POST(req: Request) {
   const eventType = evt.type;
  
   if (eventType === 'user.created'){
-    const {id,email_addresses,image_url,first_name,last_name,username} = evt.data;
+    const {id, email_addresses, image_url, first_name,last_name,username} = evt.data;
 
     const user = {
       clerkId: id,
@@ -67,7 +67,7 @@ export async function POST(req: Request) {
     }
 
 
-    const newUser = await createUser(user)
+    const newUser = await createUser(user);
     if(newUser){
       await clerkClient.users.updateUserMetadata(id,{
         publicMetadata:{
